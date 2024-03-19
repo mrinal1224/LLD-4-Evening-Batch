1ea733de566bf6a1ce8d4852cb39a06ecb424397
mrinal1224 <iammrinalkc@gmail.com>
2024-03-19 23:02:07 +0530

    fs os and path

--- class-1(Intro to Node)/node_inbuilt_modules/1_fs.js
+++ class-1(Intro to Node)/node_inbuilt_modules/1_fs.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const path = require('path')
 
 // Read a file , write a file , append a file , Delete a file
 
@@ -50,7 +51,65 @@ const fs = require('fs')
 
  // delete a file
 
- fs.unlinkSync('f2.txt')
+//  fs.unlinkSync('f2.txt')
+
+
+ // Fs module for directories
+
+ // create a dir
+
+//  fs.mkdirSync('myDirectory1') // create a directory
+//  fs.mkdirSync('myDirectory2') 
+//  fs.mkdirSync('myDirectory3') 
+//  fs.mkdirSync('myDirectory5') 
+
+// delete a dir
+
+// fs.rmdirSync('myDirectory2')
+
+
+// we need to check if the file/folder exists or not?
+
+// const doesExist = fs.existsSync('f7.txt')
+// console.log(doesExist)
+
+
+// I want to copy one file from one folder to another folder
+
+
+// srcFilePath , DestinationFolder
+
+
+let srcFilePath = '/Users/admin/Desktop/LLD-4 Evening Batch/class-1(Intro to Node)/node_inbuilt_modules/myDirectory4/f5.txt'
+
+let destinationPath = '/Users/admin/Desktop/LLD-4 Evening Batch/class-1(Intro to Node)/node_inbuilt_modules/myDirectory1'
+
+
+let tobecopiedFileName =  path.basename(srcFilePath)
+console.log(tobecopiedFileName)
+
+
+let destPath = path.join(destinationPath , tobecopiedFileName)
+console.log(destPath)
+
+fs.copyFileSync(srcFilePath , destPath)
+console.log('File Copied')
+
+fs.unlinkSync(srcFilePath) // cut functionality
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ class-1(Intro to Node)/node_inbuilt_modules/path.js
@@ -0,0 +1,21 @@
+// /Users/admin/Desktop/LLD-4 Evening Batch/class-1(Intro to Node)/node_inbuilt_modules/f4.txt
+
+
+const path = require('path')
+
+const extensionName = path.extname('/Users/admin/Desktop/LLD-4 Evening Batch/class-1(Intro to Node)/node_inbuilt_modules/f4.txt')
+
+console.log(extensionName)
+
+const baseName = path.basename('/Users/admin/Desktop/LLD-4 Evening Batch/class-1(Intro to Node)/node_inbuilt_modules/f4.txt')
+console.log(baseName)
+
+
+
+console.log(__dirname)
+console.log(__filename)
+
+
+
+
+
