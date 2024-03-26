8fdda92fcaf12f55d4d659385e51ff52c6bba2af
mrinal1224 <iammrinalkc@gmail.com>
2024-03-26 22:43:19 +0530

    post method with urlEncoded and raw json

--- class-3/index.js
+++ class-3/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const fs = require("fs");
 
 const app = express();
 
 const products = require("./MOCK_DATA.json");
+const exp = require("constants");
 
 const PORT = 8006;
+
+app.use(express.urlencoded());
+app.use(express.json())
 
 // Routing
 
@@ -12,11 +17,10 @@ app.get("/", (req, res) => {
   res.send("Welcome to the our Shop");
 });
 
-
 // hybrid Api development
-app.get('/api/products' , (req , res)=>{
-    res.json(products)
-})
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
 
 app.get("/products", (req, res) => {
   const html = `<ul> ${products.map(
@@ -24,44 +28,36 @@ app.get("/products", (req, res) => {
   )}  </ul>
     `;
 
-    res.send(html)
+  res.send(html);
 });
-
 
 // Route parameters
 
-app.get('/api/products/:id' , (req , res)=>{
-     const id = Number(req.params.id) // 7
+app.get("/api/products/:id", (req, res) => {
+  const id = Number(req.params.id); // 7
 
-     const product = products.find((product)=> product.id === id)
-     return res.json(product)
-})
-
-
+  const product = products.find((product) => product.id === id);
+  return res.json(product);
+});
 
 // Post Put Delete
 
 // Post Method
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.post("/api/products", (req, res) => {
+  const newData = req.body;
+  products.push({ id: products.length + 1, ...newData });
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(products), (err, data) => {
+    return res.send({ status: "succesfully created", id: products.length });
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`The server is running in port ${PORT}`);
 });
+
+
+// put and delete
+
+
+
