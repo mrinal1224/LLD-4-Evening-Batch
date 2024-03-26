007addeb8371c33663dc49baa6e0afd13b5b3ce4
mrinal1224 <iammrinalkc@gmail.com>
2024-03-26 19:03:11 +0530

    push

--- class-1(Intro to Node)/server/index.js
+++ class-1(Intro to Node)/server/index.js
@@ -2,33 +2,38 @@ const http = require("http");
 const fs = require("fs");
 const url = require("url");
 
-const myServer = http.createServer((req, res) => {
-  // res.end("Hola from the server")
-  console.log(req.url);
+function serverHandler(){
+  (req, res) => {
+    // res.end("Hola from the server")
+    console.log(req.url);
+  
+    const parsedUrl = url.parse(req.url, true);
+    console.log(parsedUrl);
+  
+    const log = `${req.url} Request recieved\n`;
+    fs.appendFile("log.txt", log, (err, data) => {
+      switch (parsedUrl.pathname) {
+        case "/":
+          res.end("This is my HomePage");
+          break;
+        case "/about":
+          res.end("This is my about page");
+          break;
+        case "/contact":
+          res.end("This is my contact page");
+          break;
+        case "/products":
+          const shoe = parsedUrl.query.shoes
+          res.end("This is my product page " +"here are your shoes " + shoe);
+          break;
+        default:
+          res.end("Not Found");
+      }
+    });
+  }
+}
 
-  const parsedUrl = url.parse(req.url, true);
-  console.log(parsedUrl);
-
-  const log = `${req.url} Request recieved\n`;
-  fs.appendFile("log.txt", log, (err, data) => {
-    switch (req.url) {
-      case "/":
-        res.end("This is my HomePage");
-        break;
-      case "/about":
-        res.end("This is my about page");
-        break;
-      case "/contact":
-        res.end("This is my contact page");
-        break;
-      case "/products":
-        res.end("This is my product page");
-        break;
-      default:
-        res.end("Not Found");
-    }
-  });
-});
+const myServer = http.createServer(serverHandler);
 
 myServer.listen(8002, () => {
   console.log("Server started on port 8002");
---
+++ class-2 (Introduction to express)/express/index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app  = express()
+
+
+
+app.get('/' , (req , res)=>{
+    return res.send('Hello from the server')
+})
+
+
+app.get('/about' , (req , res)=>{
+    return res.send('this is the about Page')
+})
+
+app.get('/product' , (req , res)=>{
+    console.log(req)
+    return res.send('this is the product Page' + req.query.shoes)
+    
+})
+
+
+app.listen(8003 , ()=>{
+  console.log('Server Started at 8003')
+})
+
+
+
+
+
