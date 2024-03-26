46fbc4966af324228c4b2f0f1cad16d4b8519d00
mrinal1224 <iammrinalkc@gmail.com>
2024-03-26 23:05:57 +0530

    push

--- class-3/index.js
+++ class-3/index.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const app = express();
 
 const products = require("./MOCK_DATA.json");
-const exp = require("constants");
+
 
 const PORT = 8006;
 
@@ -47,7 +47,10 @@ app.get("/api/products/:id", (req, res) => {
 app.post("/api/products", (req, res) => {
   const newData = req.body;
   products.push({ id: products.length + 1, ...newData });
-  fs.writeFile("./MOCK_DATA.json", JSON.stringify(products), (err, data) => {
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(products), (error, data) => {
+    if(err){
+        return res.send(error)
+    }
     return res.send({ status: "succesfully created", id: products.length });
   });
 });
@@ -57,7 +60,54 @@ app.listen(PORT, () => {
 });
 
 
-// put and delete
+// put 
+
+app.put('/api/products/:id' , (req , res)=>{
+    const id = Number(req.params.id); // 7
+     const body  = req.body
+    const productIndex = products.findIndex((product) => product.id === id);
+
+
+    products[productIndex] = {id:id , ...body }
+
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(products), err => {
+        if (err) {
+          return res.status(500).json({ status: "error", message: "Failed to update product" });
+        }
+        return res.json({ status: "updated successfully", id: id });
+      });
+
+})
+
+
+//Delete
+
+app.delete('/api/products/:id' , (req , res)=>{
+    const id = Number(req.params.id); // 7
+
+    const productIndex = products.findIndex((product) => product.id === id);
+
+    products.splice(productIndex , 1)
+
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(products), err => {
+        if (err) {
+          return res.status(500).json({ status: "error", message: "Failed to update product" });
+        }
+        return res.json({ status: "Deleted successfully" });
+      });
+
+})
+
+
+
+
+
+
+
+
+
+
+
 
 
 
